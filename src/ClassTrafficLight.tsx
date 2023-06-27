74867529ecac05c6799b378118a25fd4fc046290
Garri Shtyrkin <garrishtyrkin@gmail.com>
2023-06-27 15:42:22 -0400

    solved

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,68 @@
-import { Component } from "react";
+import React, { Component } from 'react';
 
-export class ClassTrafficLight extends Component {
+// Define a new type for the state of the light
+interface LightState {
+  firstLight: string;
+  secondLight: string;
+  thirdLight: string;
+}
+
+// Define a new type for the component's state
+interface ComponentState {
+  currentIndex: number;
+}
+
+export class ClassTrafficLight extends Component<{}, ComponentState> {
+  // Define states property and its type
+  states: LightState[] = [
+    {
+      firstLight: "red",
+      secondLight: "black",
+      thirdLight: "black",
+    },
+    {
+      firstLight: "black",
+      secondLight: "black",
+      thirdLight: "green",
+    },
+    {
+      firstLight: "black",
+      secondLight: "yellow",
+      thirdLight: "black",
+    },
+  ];
+
+  constructor(props: {}) {
+    super(props);
+
+    // Initialize the state
+    this.state = {
+      currentIndex: 0,
+    };
+
+    // Bind the 'this' context to the method
+    this.nextState = this.nextState.bind(this);
+  }
+
+  nextState() {
+    this.setState((prevState) => ({
+      currentIndex: (prevState.currentIndex + 1) % this.states.length,
+    }));
+  }
+
   render() {
+    const currentState = this.states[this.state.currentIndex];
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          <div className={`circle ${currentState.firstLight}`}></div>
+          <div className={`circle ${currentState.secondLight}`}></div>
+          <div className={`circle ${currentState.thirdLight}`}></div>
         </div>
-        <button className="next-state-button">Next State</button>
+        <button onClick={this.nextState} className="next-state-button">
+          Next State
+        </button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,14 +1,51 @@
-export const FunctionalTrafficLight = () => {
+import React, { useState } from 'react';
+
+interface TrafficLightState {
+  firstLight: string;
+  secondLight: string;
+  thirdLight: string;
+}
+
+const states: TrafficLightState[] = [
+  {
+    firstLight: "red",
+    secondLight: "black",
+    thirdLight: "black"
+  },
+  {
+    firstLight: "black",
+    secondLight: "black",
+    thirdLight: "green"
+  },
+  {
+    firstLight: "black",
+    secondLight: "yellow",
+    thirdLight: "black"
+  }
+]
+
+export const FunctionalTrafficLight: React.FC = () => {
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const currentState = states[currentIndex];
+
+  const nextState = () => {
+    setCurrentIndex((currentIndex + 1) % states.length);
+  }
+
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
         {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        <div className={`circle ${currentState.firstLight}`}></div>
+        <div className={`circle ${currentState.secondLight}`}></div>
+        <div className={`circle ${currentState.thirdLight}`}></div>
       </div>
-      <button className="next-state-button">Next State</button>
+      <button onClick={() => {
+        nextState()
+      }} className="next-state-button">Next State</button>
     </div>
   );
 };
